5992a2d573ec7e0bb5b6b62be4342a54d791e1e7
Maksym <gmakskovalev@gmail.com>
2021-05-28 11:40:04 +0300

    add open as, save as buttons and fixed bugs

--- UI.py
+++ UI.py
@@ -2,70 +2,164 @@ from tkinter import *
 from tkinter.ttk import Checkbutton
 from tkinter import scrolledtext
 import logicNLP
+import time
+from tkinter import * 
+from tkinter import ttk
 
-readFile = open('savedtext.txt','r')
-readT = readFile.read()
+from tkinter.filedialog import asksaveasfile, askopenfile
+
+#readFile = open('savedtext.txt','r')
+#text = readFile.read()
 
 window = Tk()
 window.state("zoomed")
 window.title("NLP")
 
+def open_file():
+	global text
+	files = [('All Files', '*.*'), 
+         ('Python Files', '*.py'),
+         ('Text Document', '*.txt')]
+	file = askopenfile(mode='r', filetypes=files, defaultextension=files)
+	#opened = open('savedtext.txt','r').read()
+	
+	text = file.read()
+	txt.insert("0.0", text)
+	file.close()
 
-def clicked(readT,name):
+def save_file():
+	files = [('All Files', '*.*'), 
+             ('Python Files', '*.py'),
+             ('Text Document', '*.txt')]
+	file = asksaveasfile(filetypes=files, defaultextension = files )
+	#with open()
+
+
+#TODO: show time of work, save as, parameters.
+
+def clicked(text,name,space=None):
+	#try:
+	K = int(entry_txt1.get())
+	WIN = int(entry_txt2.get())
+	KROK = int(entry_txt3.get())
+
 	print("foo called: ",name)
-	if name == 'mixing_symbols_global':
-		tt = logicNLP.mixing_symbols_global(readT)
-		txt.insert("0.0", tt)
-	elif name == 'mixing_words_global':
-		tt = logicNLP.mixing_words_global(readT)
-		txt.insert("0.0", tt)
-	elif name == 'mixing_strings_global':
-		tt = logicNLP.mixing_strings_global(readT)
-		txt.insert("0.0", tt)
-	elif name == 'lok2_symb':
-		tt = logicNLP.lok2_symb(readT)
-		txt.insert('0.0', tt)
-
-
-btn1 = Button(window, text="Глобальне перемішування символів", command=lambda: clicked(readT, name='mixing_symbols_global'))
-btn2 = Button(window, text="Глобальне перемішування слів", command=lambda: clicked(readT, name='mixing_words_global'))
-btn3 = Button(window, text="Глобальне перемішування речень", command=lambda: clicked(readT, name='mixing_strings_global'))
-
-btn4 = Button(window, text="Локально 1 для символів", command=lambda: clicked(readT, name='lok2_symb'))
-btn5 = Button(window, text="Локально 1 для слів", command=lambda: clicked(readT, name='lok2_symb'))
-btn6 = Button(window, text="Локально 1 речень", command=lambda: clicked(readT, name='lok2_symb'))
-
-btn7 = Button(window, text="Локально 2 для символів", command=lambda: clicked(readT, name='lok2_symb'))
-btn8 = Button(window, text="Локально 2 для слів", command=lambda: clicked(readT, name='lok2_symb'))
-btn9 = Button(window, text="Локально 2 речень", command=lambda: clicked(readT, name='lok2_symb'))
-
-btn10 = Button(window, text="delete", command=lambda: txt.delete("0.0", END))
-
-btn10.grid(column=17, row=5)
+	start_time = time.time()
+	while True:
+		if name == 'mixing_symbols_global':
+			txt.delete("0.0",END)
+			tt = logicNLP.mixing_symbols_global(text, K, space)
+			txt.insert("0.0", tt[0:10000])
+			break
+		elif name == 'mixing_words_global':
+			txt.delete("0.0",END)
+			tt = logicNLP.mixing_words_global(text, K)
+			txt.insert("0.0", tt[0:10000])
+			break
+		elif name == 'mixing_strings_global':
+			txt.delete("0.0",END)
+			tt = logicNLP.mixing_strings_global(text, K)
+			txt.insert("0.0", tt[0:10000])
+			break
+		elif name == 'lok2_mix_symbols_within_words':
+			txt.delete("0.0",END)
+			tt = logicNLP.lok2_mix_symbols_within_words(text, K, WIN, KROK)
+			txt.insert("0.0", tt[0:10000])
+			break
+		elif name == 'lok2_mix_words_within_sentences':
+			tt = logicNLP.lok2_mix_words_within_sentences(text, K, WIN, KROK)
+			txt.delete("0.0",END)
+			txt.insert("0.0", tt[0:10000])
+			break
+		elif name == 'lok1_mix_symbols_in_words':
+			txt.delete("0.0",END)
+			tt = logicNLP.lok1_mix_symbols_in_words(text, K, WIN, KROK)
+			txt.insert("0.0", tt[0:10000])
+			break
+		elif name == 'lok1_mix_words_in_sentences':
+			txt.delete("0.0",END)
+			tt = logicNLP.lok1_mix_words_in_sentences(text, K, WIN, KROK)
+			txt.insert("0.0", tt[0:10000])
+			break
+		elif name == 'lok1_mix_sentences_in_text':
+			txt.delete("0.0",END)
+			tt = logicNLP.lok1_mix_sentences_in_text(text, K, WIN, KROK)
+			txt.insert("0.0", tt[0:10000])
+			break
+	finish_time = time.time()
+	time_shower.configure(text=(f'Time: {(finish_time-start_time):.3f}'))
+			
+	#except:
+	#		txt.delete("0.0",END)
+	#		txt.insert("0.0", "Введіть змінні")
 
 chk_state = BooleanVar()
 chk_state.set(False)
 chk = Checkbutton(window, text="Враховувати пробіли", var=chk_state)
-chk.grid(column=13, row=7)
-
-txt = scrolledtext.ScrolledText(window,width=100,height=40)
-txt.insert(INSERT, "")
-txt.grid(column=5, row=5)
 
 
-#btn1.grid(column=14,row=1)
-x_cor = 1100
-btn1.grid(column=10, row=10)
-'''
-btn2.place(x=x_cor,y=100)
-btn3.place(x=x_cor,y=150)
-btn4.place(x=x_cor,y=200)
-btn5.place(x=x_cor,y=250)
-btn6.place(x=x_cor,y=300)
-btn7.place(x=x_cor,y=350)
-btn8.place(x=x_cor,y=400)
-btn9.place(x=x_cor,y=450)
-'''
+txt = scrolledtext.ScrolledText(window,width=169,height=40)
+
+time_shower = Label(window,text="Working time will be here :)")
+
+
+btn1 = Button(window, text="Глобальне перемішування символів", command=lambda: clicked(text, name='mixing_symbols_global', space=chk_state.get()))
+btn2 = Button(window, text="Глобальне перемішування слів", command=lambda: clicked(text, name='mixing_words_global'))
+btn3 = Button(window, text="Глобальне перемішування речень", command=lambda: clicked(text, name='mixing_strings_global'))
+
+btn4 = Button(window, text="Локально 2 для символів", command=lambda: clicked(text, name='lok2_mix_symbols_within_words'))
+btn5 = Button(window, text="Локально 2 для слів", command=lambda: clicked(text, name='lok2_mix_words_within_sentences'))
+btn6 = Button(window, text="Локально 2 речень", command=lambda: clicked(text, name='mixing_strings_global'))
+
+btn7 = Button(window, text="Локально 1 для символів", command=lambda: clicked(text, name='lok1_mix_symbols_in_words'))
+btn8 = Button(window, text="Локально 1 для слів", command=lambda: clicked(text, name='lok1_mix_words_in_sentences'))
+btn9 = Button(window, text="Локально 1 речень", command=lambda: clicked(text, name='lok1_mix_sentences_in_text'))
+
+btn_del = Button(window, text="delete", command=lambda: txt.delete("0.0", END))
+btn_open = Button(window, text='open file', command=open_file)
+
+btn_save = Button(window, text='save as', command=save_file)
+
+lbl1 = Label(window, text="K")
+lbl2 = Label(window, text="WIN")
+lbl3 = Label(window, text="KROK")
+
+entry_txt1 = Entry(window,  width=10)
+entry_txt2 = Entry(window,  width=10)
+entry_txt3 = Entry(window,  width=10)
+
+
+lbl1.place(x=1025, y=645)
+lbl2.place(x=1115, y=645)
+lbl3.place(x=1215, y=645)
+
+entry_txt1.place(x=1000,y=675)
+entry_txt2.place(x=1100,y=675)
+entry_txt3.place(x=1200,y=675)
+
+btn1.place(x=350,y=650)
+btn2.place(x=350,y=680)
+btn3.place(x=350,y=710)
+btn4.place(x=600,y=650)
+btn5.place(x=600,y=680)
+btn6.place(x=600,y=710)
+btn7.place(x=800,y=650)
+btn8.place(x=800,y=680)
+btn9.place(x=800,y=710)
+
+btn_del.place(x=1000,y=710)
+
+btn_open.place(x=1200,y=710)
+
+btn_save.place(x=1200,y=690)
+
+chk.place(x=200, y=653)
+
+time_shower.place(x=200, y=670)
+txt.place(x=1,y=1)
+
+
+
+
 
 window.mainloop()
-readT = readFile.read()
--- logicNLP.py
+++ logicNLP.py
@@ -4,22 +4,16 @@ import time
 import random as rnd
 
 
-N = 1
-K = 10
-WIN = 10
-KROK = 30
-
-
 readFile = open('savedtext.txt','r')
 
 
-def mixing_symbols_global(text,space=False):
+def mixing_symbols_global(text, K, space=False):
 	# test done!
 	text = text.replace(".",'')
 	text = list(text)
 
 
-	for i in range(1):
+	for i in range(K):
 		while True:
 			x1 = rnd.randint(0, len(text)-1)
 			x2 = rnd.randint(0, len(text)-1)
@@ -38,35 +32,31 @@ def mixing_symbols_global(text,space=False):
 
 
 
-def mixing_words_global(text):
+def mixing_words_global(text, K):
 	# test done!
-
+	
+	start = time.time()
 	text = text.replace(".",'')
 	text = text.split(" ")
 
-	print(text)
-
-	for i in range(N):
+	for i in range(K):
 		x1 = rnd.randint(0, len(text)-1)
 		x2 = rnd.randint(0, len(text)-1)
 
 		text[x1], text[x2] = text[x2], text[x1]
-	print(text)
 	fileforsave = open('testtext.txt','w')
 	fileforsave.write(' '.join(text))
 
 	fileforsave.close()
+	print("time: ", time.time() - start)
 	return open('testtext.txt','r').read()
 
-def mixing_strings_global(text):
-	# 
-	print(text)
+def mixing_strings_global(text, K):
+	# test done!
 	text = text.split(".")
 
-	for i in range(3):
-		print(len(text))
+	for i in range(K):
 		x1 = rnd.randint(0, len(text)-1)
-		print(x1)
 		x2 = rnd.randint(0, len(text)-1)
 
 		text[x1], text[x2] = text[x2], text[x1]
@@ -80,16 +70,21 @@ def mixing_strings_global(text):
 Далі ф-ції для 2-локального перемішування
 """
 
-def lok2_mix_symbols_within_words(text):
+def lok2_mix_symbols_within_words(text, K, WIN, KROK):
 	#Локально 2, перемішуємо символи в межах слова
+	# test done!
 	text = text.replace(".",'')
 	text = text.split(" ")
 	for i in range(len(text)):
 		text[i] = list(text[i])
 
 	for x in text:
-		for k in range(1):
+		if len(x) in [0,1]:
+			continue
+		for k in range(K):
+			print("len: ", len(x))
 			x1 = rnd.randint(0,len(x)-1)
+			print(x1)
 			x2 = rnd.randint(0,len(x)-1)
 			x[x1],x[x2] = x[x2],x[x1]
 	for i in range(len(text)):
@@ -100,25 +95,10 @@ def lok2_mix_symbols_within_words(text):
 	fileforsave.close()
 	return open('testtext.txt','r').read()
 			
-
-			
-	# 	'''
-	# 	for y in x:
-	# 		for z in range(1):
-	# 			x1 = rnd.randint(0,len(x)-1)
-	# 			x2 = rnd.randint(0,len(x)-1)
-	# 		x[x1], x[x2] = x[x2], x[x1]
-	# 	'''
-
-	# # for i in range(len(text)):
-	# # 	text[i] = ''.join(text[i])
-	# # print(text[-1])
-	# # return ' '.join(text)
-
 	
-def lok2_mix_words_within_sentences(text):
+def lok2_mix_words_within_sentences(text, K, WIN, KROK):
 	#Локально 2, перемішуємо слова в межах речення
-
+	# test done!
 	text = text.split(". ")
 
 	for i in range(len(text)):
@@ -142,16 +122,25 @@ def lok2_mix_words_within_sentences(text):
 """
 Далі ф-ції для 1-локального пермішування
 """
-def lok1_mix_symbols_in_words(text):
+def lok1_mix_symbols_in_words(text, K, WIN, KROK):
 	#time 782s for K=100000
+
 
 	text = list(text.replace(',','').replace('.',' '))
 
 	for i in range(KROK,len(text),KROK):
-		for z in range(10):
+		for z in range(K):
+			x = 0
 			while True:
-				x1 = rnd.randint(i-WIN, i+WIN)
-				x2 = rnd.randint(i-WIN, i+WIN)
+				x+=1
+				x1 = rnd.randint(i-WIN, i+WIN-1)
+				x2 = rnd.randint(i-WIN, i+WIN-1)
+				print(x)
+				# print(x1,x2)
+				# print(len(text))
+				# print(text[x1], text[x2])
+				if x == 5:
+					break
 				if text[x1] != " " and text[x2] != " ":
 					break
 			text[x1], text[x2] = text[x2], text[x1]
@@ -161,8 +150,10 @@ def lok1_mix_symbols_in_words(text):
 	fileforsave.close()
 	return open('testtext.txt','r').read()
 
-def lok1_mix_words_in_sentences(text):
+def lok1_mix_words_in_sentences(text, K, WIN, KROK):
 	# 214s for K = 10000
+	# test done!
+
 	text = text.split(" ")
 
 	for words in range(KROK,len(text),KROK):
@@ -180,8 +171,9 @@ def lok1_mix_words_in_sentences(text):
 	return open('testtext.txt','r').read()
 
 
-def lok1_mix_sentences_in_text(text):
+def lok1_mix_sentences_in_text(text, K, WIN, KROK):
 	# for K=100000 time is 97s
+	# test done!
 	text = text.split('. ')
 
 	for sentences in range(KROK, len(text), KROK):
@@ -202,6 +194,8 @@ readFile.close()
 
 if __name__ == "__main__":
 	readFile = open('savedtext.txt','r')
+	readT = readFile.read()
+	print(mixing_words_global(readT))
 
 	N = 100000
 	K = 10
